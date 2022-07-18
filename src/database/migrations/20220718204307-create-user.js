3df42f8f07a76d49cf449bfbecccc3ae4a923436
Matheus Teles <mteles.financas@gmail.com>
2022-07-18 18:01:22 -0300

    feat: Users table model and migration

---
+++ src/database/migrations/20220718204307-create-user.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      balance: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+};
