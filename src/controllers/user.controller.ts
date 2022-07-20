9b87673862bfa0f02eb26f42647bb7c76f909790
Matheus Teles <mteles.financas@gmail.com>
2022-07-20 18:31:00 -0300

    feat: UserController + get acct balance controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+import UserService from '../services/user.service'
+
+class UserController {
+  constructor(private userService = new UserService()) {}
+
+  public async retrieveBalance (request: Request, response: Response) {
+    const { id } = request.params;
+    const balance = await this.userService.retrieveBalance(Number(id));
+    return response.status(200).json(balance);
+  }
+}
+
+export default UserController;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,10 @@
-import Users from "../models/UserModel";
+import Users from "../database/models/UserModel"
 
 class UserService {
   public async retrieveBalance(id: number) {
     const entry = await Users.findOne( { where: { id } });
     return { codCliente: entry?.id, balance: entry?.balance, }
   }
-
 }
 
 export default UserService;
