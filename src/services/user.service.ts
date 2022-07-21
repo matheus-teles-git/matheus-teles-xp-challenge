49e68ebd11bb219a969beb44ca345dbfcceeecad
Matheus Teles <mteles.financas@gmail.com>
2022-07-20 21:54:25 -0300

    feat: deposit service + interface

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,21 @@
 import Users from "../database/models/UserModel"
+import IUserRequest from "../interfaces/userRequest.interface";
+
+
 
 class UserService {
   public async retrieveBalance(id: number) {
     const entry = await Users.findOne( { where: { id } });
-    return { codCliente: entry?.id, balance: entry?.balance, }
+    return { clientCode: entry?.id, balance: entry?.balance } as IUserRequest;
+  }
+
+  public async deposit(payload: IUserRequest) {
+    const current = await Users.findOne( { where: { id: payload.clientCode } });
+    if (current?.balance != undefined) {
+      const newBalance = current?.balance + payload.balance;
+      await Users.update( { balance: newBalance }, { where: { id: payload.clientCode }});
+      return { clientCode: payload.clientCode, balance: newBalance } as IUserRequest;
+    }
   }
 }
 
