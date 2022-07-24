1f48d462458e15757715b48ab4fd58a7fefb475b
Matheus Teles <mteles.financas@gmail.com>
2022-07-23 22:46:17 -0300

    feat: authentication controller

---
+++ src/controllers/authentication.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import AuthenticationService from '../services/authentication.service';
+
+class AuthenticationController {
+  constructor(private authenticationService = new AuthenticationService()) {}
+
+  public login = async (request: Request, response: Response) => {
+    const payload = request.body;
+    const login = await this.authenticationService.authenticate(payload);
+    if (login === null) {
+      return response.status(403).json({ message: 'Incorrect Email or Password' });
+    }
+    return response.status(200).json({ token });
+  }
+
+}
+
+export default AuthenticationController;
