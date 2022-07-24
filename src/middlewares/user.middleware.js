70cdc443949c5425af168f22184a60bb3c5999fe
Matheus Teles <mteles.financas@gmail.com>
2022-07-24 14:14:56 -0300

    chore: clearing scripts

--- src/middlewares/user.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function userValidation(request, response, next) {
-    const { balance } = request.body;
-    if (balance <= 0)
-        return response.status(422).json({ message: 'Operation Value can\'t be equal or less than zero ' });
-    next();
-}
-exports.default = userValidation;
